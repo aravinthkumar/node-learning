2428f3e67a26d75dff534db213a5d5bba01c8e42
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2022-11-20 13:56:15 +0530

    arrow functions

--- play.js
+++ play.js
@@ -1,12 +1,16 @@
 
 // Primitive variables
-var name = "Aravind";
-var age = 30
-var hasHobbies = true;
+let name = "Aravind";
+let age = 30
+let hasHobbies = true;
 // Function are defined 
-function summarizeUser(userName, userAge, userHasHobbies) {
+const summarizeUser = (userName, userAge, userHasHobbies) => {
     return "User is " + userName + " Age is " + userAge + " Has Hobbies " + userHasHobbies;
     // The input is not available outside the scope of the function
 }
+const addNumber = (a, b) => a + b
+
+
+console.log(addNumber(10, 2));
 
 console.log(summarizeUser(name, age, hasHobbies));
