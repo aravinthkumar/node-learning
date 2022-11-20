054986d5fde01785859650240487c9ff566066bc
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2022-11-20 18:15:24 +0530

    promise

--- play.js
+++ play.js
@@ -1,29 +1,24 @@
 
-const fetchData = (callback) => {
-    setTimeout(() => {
-        callback('Done!');
-    }, 1000);
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!');
+        }, 1000);
+    });
+    return promise;
 };
 
-const data = () => {
-    setTimeout(() => {
-        console.log('Time is done!');
-        fetchData(text => {
-            console.log(text);
+
+setTimeout(() => {
+    console.log('Time is done!');
+    fetchData().then(text => {
+        console.log(text);
+        return fetchData();
+    })
+        .then(text2 => {
+            console.log(text2);
         });
-    }, 1000);
-};
+}, 1000);
 
 console.log('Log once')
-data();
-
-const person = {
-    name: "Aravinth",
-    age: 20,
-    getPerson: function () {
-        console.log("Hi this is " + this.name);
-    }
-}
-
-const { name, age } = person;
 
