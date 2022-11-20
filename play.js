1ad8c266bfb2a334b8d1bdb4e3be2e9ee1e4b695
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2022-11-20 14:11:30 +0530

    objects in js

--- play.js
+++ play.js
@@ -1,16 +1,9 @@
-
-// Primitive variables
-let name = "Aravind";
-let age = 30
-let hasHobbies = true;
-// Function are defined 
-const summarizeUser = (userName, userAge, userHasHobbies) => {
-    return "User is " + userName + " Age is " + userAge + " Has Hobbies " + userHasHobbies;
-    // The input is not available outside the scope of the function
+const person = {
+    name: "Aravinth",
+    age: 20,
+    getPerson() {
+        console.log("Hi this is " + this.name);
+    }
 }
-const addNumber = (a, b) => a + b
 
-
-console.log(addNumber(10, 2));
-
-console.log(summarizeUser(name, age, hasHobbies));
+person.getPerson();
