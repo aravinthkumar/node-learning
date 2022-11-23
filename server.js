de553d84142379ffe47167c422cb8240cde7db50
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2022-11-23 21:56:55 +0530

    converted the router and server to express

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const http = require('http');
-const router = require('./router');
+const express = require('express');
 
-const server = http.createServer(router);
+const app = express();
+
+app.use('/users', (req, res, next) => {
+    console.log('Entering middleware!!');
+    res.send('<h1>You reached users end-point</h1>')
+});
+
+app.use('/', (req, res, next) => {
+    console.log('Entering another middleware!!');
+    res.send('<h1>Hello from Express.js</h1>')
+});
+
 console.log("Server is starting");
-server.listen(3000);
+app.listen(3000);
 console.log("Server is started and listening");
