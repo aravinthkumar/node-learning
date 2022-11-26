63d9d28b2b230be4a90b6e25a1e81d48d3c39346
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2022-11-26 16:05:47 +0530

    submitting the second assignment - contains nodemon package - / and /user endpoints - main.css in public folder - welcome and user html files

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
-app.use(shopRoutes);
+app.get('/user', (req, res, next) => {
+    console.log("/user is reached")
+    res.sendFile(path.join(__dirname, 'views', 'user.html'))
+})
+
 
 app.use('/', (req, res, next) => {
     console.log("/ end-point is reached");
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.sendFile(path.join(__dirname, 'views', 'welcome.html'));
 })
+
 
 console.log("Server is starting");
 app.listen(3000);
